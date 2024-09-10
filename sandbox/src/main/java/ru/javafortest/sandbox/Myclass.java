8691fb0009bb2c1a04b98b8682b89bc6820aaeb3
Clansman5000 <loki86mu@list.ru>
2024-09-10 18:53:56 +0300

    new stylke

--- sandbox/src/main/java/ru/javafortest/sandbox/Myclass.java
+++ sandbox/src/main/java/ru/javafortest/sandbox/Myclass.java
@@ -1,9 +1,9 @@
 package ru.javafortest.sandbox;
 
 public class Myclass {
-public static void main (String[] args)
- {System.out.println("Hellow world");}
-	
-	
-	
+    public static void main(String[] args) {
+        System.out.println("Hellow world");
+    }
+
+
 }
